3d5218c8d370f5a416a3cade4a3f192afd8f225f
smansayeed <786saadman@gmail.com>
2024-10-10 13:55:47 +0600

    Uploading app.js

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 require('dotenv').config(); // Load environment variables
 
+Object.keys(require.cache).forEach(function(key) {
+  delete require.cache[key];
+});
+
 const mainRoutes = require('./routes/mainRoutes');
 const powerRoutes = require('./routes/powerRoutes');
-
+const userRoutes = require('./routes/userRoutes');
+const dataRoutes = require('./routes/dataRoutes');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Routes
 app.use('/api/main', mainRoutes);
 app.use('/api/power', powerRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/data', dataRoutes); 
 
 // Start the server
 app.listen(port, () => {
